5be49615b0861bfe9b0ab1ce60ba556725ce091c
CraterMaik <cratermaik@gmail.com>
2021-08-23 18:59:41 -0500

    Update App Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import AwesomeSlider from 'react-awesome-slider';
-import withAutoplay from 'react-awesome-slider/dist/autoplay';
-
 import LayoutMain from './component/layout/Main';
-import Hero from './component/sections/Hero';
+import Slider from './component/sections/SliderSection';
 import OptionsSection from './component/sections/OptionsSection';
-
-import 'react-awesome-slider/dist/custom-animations/fall-animation.css';
-import 'react-awesome-slider/dist/styles.css';
-
-const AutoplaySlider = withAutoplay(AwesomeSlider);
-
-
+import Hero from './component/sections/Hero';
 
 function App() {
   return (
     <LayoutMain>
-      <AutoplaySlider play={true}
-        cancelOnInteraction={false} 
-        interval={6000}
-        style={{ height:'650px' }}>
-        
-        <div data-src="https://i.imgur.com/rH7NmFY.gif" />
-        <div data-src="https://www.metroshoes.net/blog/wp-content/uploads/2019/08/770-X-400-Pixel-Leather-Care-Banner.jpg" />
-        <div data-src="https://cdn.shopify.com/s/files/1/0419/1525/collections/2800x890-Women-Care-Leather_2800x890.jpg?v=1534316025" />
-        <div data-src="https://www.greywalkshoes.com/front/img/grey-banner-new.webp" />
-      </AutoplaySlider>
+      <Slider />
       <OptionsSection />
       <Hero />
     </LayoutMain>
