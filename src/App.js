51d1f175863b235b856dba890c6bd413cdd14973
CraterMaik <cratermaik@gmail.com>
2021-08-23 19:36:51 -0500

    Update App Products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LayoutMain from './component/layout/Main';
 import Slider from './component/sections/SliderSection';
 import OptionsSection from './component/sections/OptionsSection';
+import ProductsSection from './component/sections/ProductsSection';
 import Hero from './component/sections/Hero';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <LayoutMain>
       <Slider />
       <OptionsSection />
+      <ProductsSection />
       <Hero />
     </LayoutMain>
   );
--- src/component/sections/OptionsSection.js
+++ src/component/sections/OptionsSection.js
@@ -2,12 +2,23 @@
 const products = [
   {
     id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0367/8627/4441/files/atributo_1_160x160@2x.jpg?v=1625846701',
+    imageAlt: "img-1",
+  },
+  {
+    id: 2,
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0367/8627/4441/files/atributo_2_160x160@2x.jpg?v=1625846720',
+    imageAlt: "img-2",
+  },
+  {
+    id: 3,
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0367/8627/4441/files/atributo_3_160x160@2x.jpg?v=1625846737',
+    imageAlt: "img-3",
+  },
+  {
+    id: 4,
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0367/8627/4441/files/atributo_4_160x160@2x.jpg?v=1625846756',
+    imageAlt: "img-4",
   },
   // More products...
 ]
@@ -15,31 +26,19 @@ const products = [
 export default function OptionsSection() {
   return (
     <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
-
+      <div className="max-w-2xl mx-auto py-10 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <div key={product.id} className="group relative">
-              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <div className="w-full rounded-md overflow-hidden group-hover:opacity-75 cursor-pointer">
                 <img
                   src={product.imageSrc}
                   alt={product.imageAlt}
-                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                  className="w-full object-center lg:w-full sm:w-1/4"
                 />
               </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-              </div>
+              
             </div>
           ))}
         </div>
