959e2355ae61d1ff48a2c247987a4e590f4ef9b7
CraterMaik <cratermaik@gmail.com>
2021-09-07 22:03:06 -0500

    Update sections

--- src/component/sections/ClientsSection.js
+++ src/component/sections/ClientsSection.js
@@ -32,6 +32,7 @@ export default function ClientsSection() {
   return (
     <div className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        
         <div className="lg:text-center">
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             Nuestros Clientes
@@ -58,6 +59,7 @@ export default function ClientsSection() {
             ))}
           </dl>
         </div>
+
       </div>
     </div>
   )
